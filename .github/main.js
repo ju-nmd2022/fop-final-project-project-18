48d667e39997e220c3b25e1bc8ed2981921f01ec
Eliaswalle <wael22nq@student.ju.se>
2023-04-13 16:32:33 +0200

    bushes

--- .github/main.js
+++ .github/main.js
@@ -1,23 +1,60 @@
+let x = 100;
+let y = 100;
+let s = 1.0;
+
 function startScreen() {
   fill(126, 200, 80);
   rect(0, 0, 800);
+}
+
+function bush(x, y, s) {
+  fill(0, 120, 0);
+  rect(x, y + 100 * s, 40 * s, 40 * s);
+  rect(x + 40 * s, y + 100 * s, 40 * s, 40 * s);
+  fill(0, 130, 0);
+  rect(x + 80 * s, y + 100 * s, 40 * s, 40 * s);
+  fill(0, 110, 0);
+  rect(x + 120 * s, y + 100 * s, 40 * s, 40 * s);
+  fill(0, 120, 0);
+  rect(x + 160 * s, y + 100 * s, 40 * s, 40 * s);
+  fill(0, 111, 0);
+  rect(x + 20 * s, y + 60 * s, 40 * s, 40 * s);
+  rect(x + 60 * s, y + 60 * s, 40 * s, 40 * s);
+  fill(0, 120, 0);
+  rect(x + 100 * s, y + 60 * s, 40 * s, 40 * s);
+  rect(x + 140 * s, y + 60 * s, 40 * s, 40 * s);
+  fill(0, 130, 0);
+  rect(x + 60 * s, y + 20 * s, 40 * s, 40 * s);
+  rect(x + 100 * s, y + 20 * s, 40 * s, 40 * s);
+  fill(0, 110, 0);
+  rect(x + 70 * s, y + 115 * s, 25 * s, 25 * s);
+  rect(x + 110 * s, y + 115 * s, 25 * s, 25 * s);
 }
 function gameScreen() {
   fill(126, 200, 80);
   rect(0, 0, 800);
   noStroke();
   fill(217, 179, 130);
-  rect(340, 0, 60, 1000);
   //the right path
   rect(400, 200, 190, 40);
   rect(400, 350, 190, 40);
   rect(400, 500, 190, 40);
   //path in the middle
-  rect(340, 0, 60, 1000);
+  rect(340, 0, 60, 800);
   //the left path
   rect(150, 200, 190, 40);
   rect(150, 350, 190, 40);
   rect(150, 500, 190, 40);
+  //all the bushes: (in order with Y-value)
+  bush(200, 100, 0.4);
+  bush(500, 200, 0.4);
+  bush(100, 300, 0.4);
+  bush(500, 300, 0.4);
+  bush(260, 300, 0.4);
+  bush(250, 400, 0.4);
+  bush(450, 400, 0.4);
+  bush(100, 550, 0.4);
+  bush(440, 590, 0.4);
 }
 function leaderBoardScreen() {
   rect(100, 100, 10);
@@ -31,5 +68,7 @@ function draw() {
     gameScreen();
   } else if (state === "leaderBoard") {
     leaderBoardScreen();
+  } else if (state === "bush") {
+    bush();
   }
 }
