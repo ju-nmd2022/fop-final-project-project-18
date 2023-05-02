3a0d3c6dcd9bb23e9889e41a8f8ba03e68d4da33
Eliaswalle <wael22nq@student.ju.se>
2023-05-03 00:06:24 +0200

    character finished and moving

--- .github/main.js
+++ .github/main.js
@@ -1,22 +1,40 @@
 let x = 100;
 let y = 100;
 let s = 1.0;
+let playerX = 200;
+let playerY = 200;
 
 function startScreen() {
   fill(126, 200, 80);
   rect(0, 0, 800);
 }
 function playerOne(x, y, s) {
+  if (keyIsDown(LEFT_ARROW)) {
+    // if the left arrow key is pressed
+    x -= 5; // move playerOne 5 pixels to the left
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    // if the right arrow key is pressed
+    x += 5; // move playerOne 5 pixels to the right
+  }
+  if (keyIsDown(UP_ARROW)) {
+    // if the up arrow key is pressed
+    y -= 5; // move playerOne 5 pixels up
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    // if the down arrow key is pressed
+    y += 5; // move playerOne 5 pixels down
+  }
   push();
-  strokeWeight(2);
+  strokeWeight(0);
   // beige foot
   fill(217, 179, 130);
-  rect(x - 20, y + 400 * s, 40 * s, 20 * s);
-  rect(x + 100, y + 400 * s, 40 * s, 20 * s);
+  rect(x - 20 * s, y + 400 * s, 40 * s, 20 * s);
+  rect(x + 100 * s, y + 400 * s, 40 * s, 20 * s);
   // loght blue legs
   fill(0, 220, 255);
-  rect(x - 20, y + 380 * s, 60 * s, 20 * s);
-  rect(x + 100, y + 380 * s, 40 * s, 20 * s);
+  rect(x - 20 * s, y + 380 * s, 60 * s, 20 * s);
+  rect(x + 100 * s, y + 380 * s, 40 * s, 20 * s);
   //dark blue legs
   fill(0, 0, 100);
   rect(x, y + 360 * s, 70 * s, 20 * s);
@@ -44,8 +62,71 @@ function playerOne(x, y, s) {
   rect(x + 15 * s, y + 70 * s, 160 * s, 60 * s);
   rect(x + 60 * s, y + 50 * s, 90 * s, 20 * s);
   // eyes
+  fill(255, 255, 255);
+  rect(x + 50 * s, y + 150 * s, 25 * s, 25 * s);
+  rect(x + 110 * s, y + 150 * s, 25 * s, 25 * s);
+  fill(0, 0, 0);
+  rect(x + 75 * s, y + 150 * s, 25 * s, 25 * s);
+  rect(x + 135 * s, y + 150 * s, 25 * s, 25 * s);
 
+  //mouth
+  fill(255, 179, 179);
+  rect(x + 80 * s, y + 200 * s, 40 * s, 10 * s);
   pop();
+  playerX = x; // update x and y to the new position
+  playerY = y;
+}
+function reversedPlayerOne(x, y, s) {
+  push();
+  strokeWeight(0);
+  // beige foot
+  fill(217, 179, 130);
+  rect(x - 20 * s, y + 400 * s, 40 * s, 20 * s);
+  rect(x + 100 * s, y + 400 * s, 40 * s, 20 * s);
+  // loght blue legs
+  fill(0, 220, 255);
+  rect(x - 20 * s, y + 380 * s, 60 * s, 20 * s);
+  rect(x + 100 * s, y + 380 * s, 40 * s, 20 * s);
+  //dark blue legs
+  fill(0, 0, 100);
+  rect(x, y + 360 * s, 70 * s, 20 * s);
+  rect(x + 30 * s, y + 340 * s, 40 * s, 40 * s);
+  rect(x + 100 * s, y + 340 * s, 40 * s, 40 * s);
+  rect(x + 30 * s, y + 330 * s, 110 * s, 30 * s);
+  //red body
+  fill(255, 0, 0);
+  rect(x + 30 * s, y + 230 * s, 110 * s, 100 * s);
+
+  //the body
+  fill(217, 179, 130);
+  rect(x - 10 * s, y + 290 * s, 40 * s, 40 * s);
+  rect(x + 10 * s, y + 270 * s, 40 * s, 20 * s);
+  rect(x + 140 * s, y + 290 * s, 40 * s, 40 * s);
+  rect(x + 140 * s, y + 270 * s, 20 * s, 20 * s);
+  rect(x + 50 * s, y + 150 * s, 110 * s, 80 * s);
+  //hair
+  fill(0, 0, 0);
+  rect(x + 30 * s, y + 150 * s, 20 * s, 40 * s);
+  rect(x + 10 * s, y + 150 * s, 30 * s, 20 * s);
+  //hat
+  fill(230, 120, 0);
+  rect(x - 10 * s, y + 130 * s, 220 * s, 20 * s);
+  rect(x + 15 * s, y + 70 * s, 160 * s, 60 * s);
+  rect(x + 60 * s, y + 50 * s, 90 * s, 20 * s);
+  // eyes
+  fill(255, 255, 255);
+  rect(x + 50 * s, y + 150 * s, 25 * s, 25 * s);
+  rect(x + 110 * s, y + 150 * s, 25 * s, 25 * s);
+  fill(0, 0, 0);
+  rect(x + 75 * s, y + 150 * s, 25 * s, 25 * s);
+  rect(x + 135 * s, y + 150 * s, 25 * s, 25 * s);
+
+  //mouth
+  fill(255, 179, 179);
+  rect(x + 80 * s, y + 200 * s, 40 * s, 10 * s);
+  pop();
+  playerX = x; // update x and y to the new position
+  playerY = y;
 }
 function lawnMower(x, y, s) {
   // lawnmower body
@@ -93,7 +174,7 @@ function gameScreen() {
   fill(126, 200, 80);
   rect(0, 0, 800);
   noStroke();
-  fill(217, 179, 130);
+  fill(147, 179, 130);
   //the right path
   rect(400, 200, 190, 40);
   rect(400, 350, 190, 40);
@@ -116,7 +197,8 @@ function gameScreen() {
   bush(100, 550, 0.7);
   bush(440, 590, 0.4);
   lawnMower(600, 200, 0.7);
-  playerOne(100, 100, 1.0);
+  playerOne(playerX, playerY, 0.2);
+  reversedPlayerOne(playerX, playerY, 0.9);
 }
 function leaderBoardScreen() {
   rect(100, 100, 10);
@@ -130,7 +212,45 @@ function draw() {
     gameScreen();
   } else if (state === "leaderBoard") {
     leaderBoardScreen();
+    playerOne();
   } else if (state === "bush") {
     bush();
   }
 }
+
+// let x = 100; // initialize the x position of playerOne
+// let y = 100; // initialize the y position of playerOne
+// let state = "game"; // initialize the game state
+
+// function draw() {
+//   background(220); // set the background to white
+//   if (state === "game") {
+//     drawGame(); // draw the game screen
+//     movePlayerOne(); // update the position of playerOne based on user input
+//   }
+// }
+
+// function drawGame() {
+//   // draw the game screen here
+//   rect(0, 0, width, height); // draw a rectangle that covers the whole canvas
+//   drawPlayerOne(); // draw playerOne at its current position
+// }
+
+// function drawPlayerOne() {
+//   rect(x, y, 50, 50); // draw playerOne as a rectangle
+// }
+
+// function movePlayerOne() {
+//   if (keyIsDown(LEFT_ARROW)) { // if the left arrow key is pressed
+//     x -= 5; // move playerOne 5 pixels to the left
+//   }
+//   if (keyIsDown(RIGHT_ARROW)) { // if the right arrow key is pressed
+//     x += 5; // move playerOne 5 pixels to the right
+//   }
+//   if (keyIsDown(UP_ARROW)) { // if the up arrow key is pressed
+//     y -= 5; // move playerOne 5 pixels up
+//   }
+//   if (keyIsDown(DOWN_ARROW)) { // if the down arrow key is pressed
+//     y += 5; // move playerOne 5 pixels down
+//   }
+// }
