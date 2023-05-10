262e2f26a4d38db3627166001e80331c6e2d4770
Eliaswalle <wael22nq@student.ju.se>
2023-05-10 21:45:47 +0200

    flowers

--- .github/main.js
+++ .github/main.js
@@ -3,6 +3,7 @@ let y = 100;
 let s = 1.0;
 let playerX = 200;
 let playerY = 200;
+let move = 5;
 
 function startScreen() {
   fill(126, 200, 80);
@@ -10,16 +11,17 @@ function startScreen() {
 }
 function playerOne(x, y, s) {
   if (keyIsDown(LEFT_ARROW)) {
-    x -= 5;
+    x -= move;
   } else if (keyIsDown(RIGHT_ARROW)) {
-    x += 5;
+    x += move;
   }
   if (keyIsDown(UP_ARROW)) {
-    y -= 5;
+    y -= move;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    y += 5;
+    y += move;
   }
+
   push();
   strokeWeight(0);
   // beige foot
@@ -165,6 +167,24 @@ function bush(x, y, s) {
   rect(x + 70 * s, y + 115 * s, 25 * s, 25 * s);
   rect(x + 110 * s, y + 115 * s, 25 * s, 25 * s);
 }
+function flowers(x, y, s) {
+  fill(0, 255, 0);
+  noStroke();
+  rect(x + 200 * s, y + 200 * s, 20 * s, 190 * s);
+  rect(x + 220 * s, y + 260 * s, 20 * s, 20 * s);
+  rect(x + 240 * s, y + 220 * s, 20 * s, 40 * s);
+  rect(x + 260 * s, y + 220 * s, 20 * s, 20 * s);
+  rect(x + 180 * s, y + 290 * s, 20 * s, 40 * s);
+  rect(x + 160 * s, y + 290 * s, 20 * s, 20 * s);
+  rect(x + 140 * s, y + 270 * s, 20 * s, 20 * s);
+  fill(255, 0, 0);
+  rect(x + 180 * s, y + 150 * s, 60 * s, 50 * s);
+  rect(x + 170 * s, y + 130 * s, 40 * s, 50 * s);
+  rect(x + 210 * s, y + 130 * s, 40 * s, 50 * s);
+  rect(x + 180 * s, y + 110 * s, 60 * s, 40 * s);
+  fill(255, 255, 0);
+  rect(x + 200 * s, y + 150 * s, 20 * s, 20 * s);
+}
 function gameScreen() {
   fill(126, 200, 80);
   rect(0, 0, 800);
@@ -193,6 +213,16 @@ function gameScreen() {
   bush(440, 590, 0.4);
   lawnMower(600, 200, 0.7);
   playerOne(playerX, playerY, 0.2);
+  //flowers
+  flowers(x, y, 0.2);
+  flowers(x + 100, y + 300, 0.2);
+  flowers(x + 100, y + 300, 0.2);
+  flowers(x + 300, y + 300, 0.2);
+  flowers(x, y + 300, 0.2);
+  flowers(x + 100, y + 500, 0.2);
+  flowers(x + 400, y + 200, 0.2);
+  flowers(x + 200, y + 900, 0.2);
+  flowers(x + 500, y + 500, 0.2);
 }
 function leaderBoardScreen() {
   rect(100, 100, 10);
@@ -211,40 +241,3 @@ function draw() {
     bush();
   }
 }
-
-// let x = 100; // initialize the x position of playerOne
-// let y = 100; // initialize the y position of playerOne
-// let state = "game"; // initialize the game state
-
-// function draw() {
-//   background(220); // set the background to white
-//   if (state === "game") {
-//     drawGame(); // draw the game screen
-//     movePlayerOne(); // update the position of playerOne based on user input
-//   }
-// }
-
-// function drawGame() {
-//   // draw the game screen here
-//   rect(0, 0, width, height); // draw a rectangle that covers the whole canvas
-//   drawPlayerOne(); // draw playerOne at its current position
-// }
-
-// function drawPlayerOne() {
-//   rect(x, y, 50, 50); // draw playerOne as a rectangle
-// }
-
-// function movePlayerOne() {
-//   if (keyIsDown(LEFT_ARROW)) { // if the left arrow key is pressed
-//     x -= 5; // move playerOne 5 pixels to the left
-//   }
-//   if (keyIsDown(RIGHT_ARROW)) { // if the right arrow key is pressed
-//     x += 5; // move playerOne 5 pixels to the right
-//   }
-//   if (keyIsDown(UP_ARROW)) { // if the up arrow key is pressed
-//     y -= 5; // move playerOne 5 pixels up
-//   }
-//   if (keyIsDown(DOWN_ARROW)) { // if the down arrow key is pressed
-//     y += 5; // move playerOne 5 pixels down
-//   }
-// }
