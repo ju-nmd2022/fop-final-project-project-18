9527b7ff37d0ee1b25ce2704281028b73827f09c
Eliaswalle <wael22nq@student.ju.se>
2023-05-11 15:37:31 +0200

    flowerguy

--- .github/main.js
+++ .github/main.js
@@ -3,11 +3,89 @@ let y = 100;
 let s = 1.0;
 let playerX = 200;
 let playerY = 200;
-let move = 5;
+let move = 20;
 
 function startScreen() {
   fill(126, 200, 80);
   rect(0, 0, 800);
+}
+function playerFlower(x,y,s){
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= move;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    x += move;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    y -= move;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    y += move;
+  }
+
+  push();
+  strokeWeight(0);
+  // beige foot
+  fill(217, 179, 130);
+  rect(x - 20 * s, y + 400 * s, 40 * s, 20 * s);
+  rect(x + 100 * s, y + 400 * s, 40 * s, 20 * s);
+  // loght blue legs
+  fill(0, 220, 255);
+  rect(x - 20 * s, y + 380 * s, 60 * s, 20 * s);
+  rect(x + 100 * s, y + 380 * s, 40 * s, 20 * s);
+  //dark blue legs
+  fill(0, 0, 100);
+  rect(x, y + 360 * s, 70 * s, 20 * s);
+  rect(x + 30 * s, y + 340 * s, 40 * s, 40 * s);
+  rect(x + 100 * s, y + 340 * s, 40 * s, 40 * s);
+  rect(x + 30 * s, y + 330 * s, 110 * s, 30 * s);
+  //red body
+  fill(255, 0, 0);
+  rect(x + 30 * s, y + 230 * s, 110 * s, 100 * s);
+
+  //the body
+  fill(217, 179, 130);
+  rect(x - 10 * s, y + 290 * s, 40 * s, 40 * s);
+  rect(x + 10 * s, y + 270 * s, 40 * s, 20 * s);
+  rect(x + 140 * s, y + 290 * s, 40 * s, 40 * s);
+  rect(x + 140 * s, y + 270 * s, 20 * s, 20 * s);
+  rect(x + 50 * s, y + 150 * s, 110 * s, 80 * s);
+  //hair
+  fill(0, 0, 0);
+  rect(x + 30 * s, y + 150 * s, 20 * s, 40 * s);
+  rect(x + 10 * s, y + 150 * s, 30 * s, 20 * s);
+  //hat
+  fill(230, 120, 0);
+  rect(x - 10 * s, y + 130 * s, 220 * s, 20 * s);
+  rect(x + 15 * s, y + 70 * s, 160 * s, 60 * s);
+  rect(x + 60 * s, y + 50 * s, 90 * s, 20 * s);
+  // eyes
+  fill(255, 255, 255);
+  rect(x + 50 * s, y + 150 * s, 25 * s, 25 * s);
+  rect(x + 110 * s, y + 150 * s, 25 * s, 25 * s);
+  fill(0, 0, 0);
+  rect(x + 75 * s, y + 150 * s, 25 * s, 25 * s);
+  rect(x + 135 * s, y + 150 * s, 25 * s, 25 * s);
+
+  //mouth
+  fill(255, 179, 179);
+  rect(x + 80 * s, y + 200 * s, 40 * s, 10 * s);
+  pop();
+  //flower
+  fill(0, 255, 0);
+  noStroke();
+  rect(x + 170 * s, y + 270 * s, 10 * s, 60 * s);
+  rect(x + 180 * s, y + 280 * s, 10 * s, 10 * s);
+  rect(x + 190 * s, y + 270 * s, 10 * s, 20 * s);
+  rect(x + 200 * s, y + 260 * s, 10 * s, 10 * s);
+  fill(255, 0, 0);
+  
+  rect(x + 160 * s, y + 230 * s, 30 * s, 45 * s);
+  rect(x + 160 * s, y + 250 * s, 30 * s, 20 * s);
+  fill(255, 255, 0);
+  rect(x + 170 * s, y + 245 * s, 10 * s, 10 * s);
+
+  playerX = x; // update x and y to the new position
+  playerY = y;
 }
 function playerOne(x, y, s) {
   if (keyIsDown(LEFT_ARROW)) {
@@ -70,6 +148,7 @@ function playerOne(x, y, s) {
   fill(255, 179, 179);
   rect(x + 80 * s, y + 200 * s, 40 * s, 10 * s);
   pop();
+
   playerX = x; // update x and y to the new position
   playerY = y;
 }
@@ -212,7 +291,6 @@ function gameScreen() {
   bush(100, 550, 0.7);
   bush(440, 590, 0.4);
   lawnMower(600, 200, 0.7);
-  playerOne(playerX, playerY, 0.2);
   //flowers
   flowers(x, y, 0.2);
   flowers(x + 100, y + 300, 0.2);
@@ -223,11 +301,17 @@ function gameScreen() {
   flowers(x + 400, y + 200, 0.2);
   flowers(x + 200, y + 900, 0.2);
   flowers(x + 500, y + 500, 0.2);
+playerFlower(playerX,playerY,0.2);
+playerOne(playerX, playerY, 0.2);
 }
 function leaderBoardScreen() {
   rect(100, 100, 10);
 }
 
+function keyIsPressed(){
+}
+function keyReleased(){
+}
 let state = "game";
 function draw() {
   if (state === "start") {
@@ -241,3 +325,4 @@ function draw() {
     bush();
   }
 }
+
